7e060de5033e97d29b686382eb6369ac184feacf
Joel Wachsler <joel@wachsler.se>
2018-02-07 14:27:10 +0100

    Change navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,12 @@
 <template>
-  <v-toolbar dark color="primary">
-    <v-toolbar-side-icon />
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
-    <v-spacer />
-    <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>apps</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>refresh</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>more_vert</v-icon>
-    </v-btn>
+  <v-toolbar>
+    <v-toolbar-title>Recruitment application</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
